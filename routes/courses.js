48b8bf8cdaba4f9a7c7a535eee41313c82643874
AditapKumar <aditaprawal2004@gmail.com>
2025-06-29 19:33:43 +0530

    Updated

--- routes/courses.js
+++ routes/courses.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = require("router");
 const mongoose = require("mongoose");
 
+// Fetching Course From DB
 router.get("/", (req, res) => {
   const courses = Course.find();
   res.send(courses);
